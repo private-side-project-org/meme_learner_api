9e445e1c3c719451f2b626cbe6189a3eefd8a86b
gonakano <g.nakano555@gmail.com>
2021-12-26 22:28:03 +0900

    added comment

--- cmd/api/meme-handler.go
+++ cmd/api/meme-handler.go
@@ -8,11 +8,10 @@ import (
 	"github.com/julienschmidt/httprouter"
 )
 
-func (app *application) getUserAndMemes(w http.ResponseWriter, r *http.Request){
+func (app *application) getUserAndMemes(w http.ResponseWriter, r *http.Request) {
 	params := httprouter.ParamsFromContext(r.Context())
 
 	id, err := strconv.Atoi(params.ByName("id"))
-
 	if err != nil {
 		app.logger.Print(errors.New("invalid id parameter"))
 		app.errorJson(w, err)
@@ -22,22 +21,26 @@ func (app *application) getUserAndMemes(w http.ResponseWriter, r *http.Request){
 	app.logger.Println("id is", id)
 
 	user, err := app.models.DB.Get(id)
+	if err != nil {
+		app.logger.Println(err)
+	}
 
 	err = app.writeJSON(w, http.StatusOK, user, "user")
 
 	if err != nil {
 		app.logger.Println(err)
 	}
-
 }
 
-func (app *application) getRandomMeme(w http.ResponseWriter, r *http.Request){
+func (app *application) getRandomMeme(w http.ResponseWriter, r *http.Request) {
 	scrapedMeme, err := app.models.DB.Scraping()
+	if err != nil {
+		app.logger.Println(err)
+	}
 
 	err = app.writeJSON(w, http.StatusOK, scrapedMeme, "scrapedMeme")
 
 	if err != nil {
 		app.logger.Println(err)
 	}
-
 }
--- cmd/api/routes.go
+++ cmd/api/routes.go
@@ -6,12 +6,17 @@ import (
 	"github.com/julienschmidt/httprouter"
 )
 
-
+// router to exec action when request happen on each path
 func (app *application) routes() http.Handler {
 	router := httprouter.New()
 
+	// exec status handler when GET request on /status
 	router.HandlerFunc(http.MethodGet, "/status", app.statusHandler)
+
+	// exec getUserAndMemes when GET request on /v1/user/:id
 	router.HandlerFunc(http.MethodGet, "/v1/user/:id", app.getUserAndMemes)
+
+	// exec getRandomMeme when GET request on /v1/memes/random
 	router.HandlerFunc(http.MethodGet, "/v1/memes/random", app.getRandomMeme)
 
 	return app.enableCORS(router)
