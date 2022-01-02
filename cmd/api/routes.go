9af1068587607dcc026b2d2e30420dbcbb849d97
gonakano <g.nakano555@gmail.com>
2022-01-02 21:46:00 +0900

    added session router

--- cmd/api/routes.go
+++ cmd/api/routes.go
@@ -9,6 +9,8 @@ import (
 // router to exec action when request happen on each path
 func (app *application) routes() http.Handler {
 	router := httprouter.New()
+	// sign in
+	router.HandlerFunc(http.MethodPost, "/v1/session", app.Signin)
 
 	// exec status handler when GET request on /status
 	router.HandlerFunc(http.MethodGet, "/status", app.statusHandler)
