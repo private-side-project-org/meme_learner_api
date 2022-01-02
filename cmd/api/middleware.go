fd6d69a0e9eab106ed491d687d12e8b7aded30a5
gonakano <g.nakano555@gmail.com>
2022-01-02 21:45:36 +0900

    prevent cors error when preFlight OPTION method

--- cmd/api/middleware.go
+++ cmd/api/middleware.go
@@ -2,10 +2,16 @@ package main
 
 import "net/http"
 
-func (app *application) enableCORS(next http.Handler) http.Handler{
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request){
-		// adding `Access-Control-Allow-Origin: *` which accepts all 
+func (app *application) enableCORS(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		// adding `Access-Control-Allow-Origin: *` which accepts all
 		w.Header().Set("Access-Control-Allow-Origin", "*")
+		w.Header().Set("Access-Control-Allow-Headers", "*")
+
+		if r.Method == "OPTIONS" {
+			w.WriteHeader(http.StatusOK)
+			return
+		}
 
 		next.ServeHTTP(w, r)
 	})
